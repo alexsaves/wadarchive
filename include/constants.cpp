f5f3cafbfd0c7f3cac91156c0288989f2593e8df
Alexei White <alexei.white@gmail.com>
2023-06-14 07:17:09 -0700

    Bumped version for clarity

--- include/constants.cpp
+++ include/constants.cpp
@@ -2,7 +2,7 @@
 
 namespace wadarchive
 {
-    const float ENGINE_VERSION = 0.01;
+    const float ENGINE_VERSION = 0.02;
     const char *ENGINE_NAME = "WadArchiver";
     const char *COPYRIGHT_STATEMENT = "(c) Copyright 2023, Alexei White - All rights reserved.";
     const char *LICENSE_STATEMENT = "Licensed to author under the Mozilla Public License Version 2.0";
@@ -10,5 +10,5 @@ namespace wadarchive
 	const int FRONT_BUFFER_LENGTH = 20;
 	const int SUFFIX_BUFFER_LENGTH = 20;
 
-	const float MIN_READABLE_ENGINE_VERSION = 0.009;
+	const float MIN_READABLE_ENGINE_VERSION = 0.01;
 }
